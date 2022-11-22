e836d0db69d3bc319d688b4e286d2b0905188d9a
Victor Pachas <manuel_ps03@hotmail.com>
2022-11-22 15:59:46 -0500

    cambios para el uso de managed identity

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 import os
 app=Flask(__name__)
 
-#connect_str = "DefaultEndpointsProtocol=https;AccountName=backupdiag836;AccountKey=fX2tmlyH+Web0GbBCY+Y1BrUhb8nsa6ltFelcPaYnccTzDXLYpX7PxJQUHI9+f06SQF0xZeMgKiQnAwXQCxWZA==;EndpointSuffix=core.windows.net"
-connect_str = os.environ.get('connect_str','')
+
+##Storage Account key access
+connect_str = "DefaultEndpointsProtocol=https;AccountName=storageweb123;AccountKey=86Y975cXnwojUoxe6T/jdBQhthxbpU3IBerUw0doKGHGLoOGqiHtJbyazPdqIbsVxY+EjRKyfQXQ+AStCbng8g==;EndpointSuffix=core.windows.net"
+#connect_str = os.environ.get('connect_str','')
+# Create the BlobServiceClient object which will be used to create a container client
+if connect_str != "":
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+
+##Storage account Managed identity Access
+#account_url = "https://<storageaccountname>.blob.core.windows.net"
+#connect_str = DefaultAzureCredential()
+#if connect_str != "":
+#    blob_service_client = BlobServiceClient(account_url, credential=connect_str)
 
 
 @app.route('/')
@@ -13,27 +25,19 @@ def home():
 
 @app.route('/blobstorage', methods=['GET','POST'])
 def blobstorage():
-
-    # Create the BlobServiceClient object which will be used to create a container client
-    if connect_str != "":
-        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    # Create a unique name for the container
-    #container_name = "demo1092933"
-    # Create the container
-    #container_client = blob_service_client.create_container(container_name)
     if request.method == "POST":
         if request.files:
             image = request.files['image']
             print (image.filename)
             # Create a blob client using the local file name as the name for the blob
-            blob_client = blob_service_client.get_blob_client(container="demo1092933", blob=image.filename)
+            blob_client = blob_service_client.get_blob_client(container="imagenes", blob=image.filename)
             # Upload the created file
             blob_client.upload_blob(image)
             
             return redirect(request.url)
 
     if connect_str != "":
-        container_client = blob_service_client.get_container_client("demo1092933")
+        container_client = blob_service_client.get_container_client("imagenes")
         list_blob=[]
         blob_list = container_client.list_blobs()
         for blob in blob_list:
@@ -45,7 +49,7 @@ def blobstorage():
         list_blob.append(('No se ha creado la variable de entorno connect_str',''))
         t=tuple(list_blob)
     print (t)
-    return render_template("blob.html", files = t)
+    return render_template("blob.html", files = t, connect_str = connect_str)
 
 @app.route('/about')
 def about():
